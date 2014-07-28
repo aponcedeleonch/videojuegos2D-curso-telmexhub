ef80728234007c01c53c53f69efa39843720d0f9
unknown <Hubber@.(none)>
2014-07-28 16:46:11 -0500

    Moidificando mapa subterraneo

--- juego/cargar_recursos.js
+++ juego/cargar_recursos.js
@@ -3,13 +3,14 @@
 //callback para configurar los sprites
 
 //Carga todos los recursos del mapa
-var recursos = "mario_muere.mp3, pausa.mp3, bump.ogg, patada.mp3, salto_enano.mp3, tema_superficie.mp3, mosaicos_escenario_32x32.png, jugador.json, mio.tmx, mosaicos_mario_enano_30x30.png, goomba.json, mosaicos_enemigos_32x32.png, mosaicos_enemigos_32x46.png, tortuga.json";
+var recursos = "subterraneo.mp3, mario_muere.mp3, pausa.mp3, bump.ogg, patada.mp3, salto_enano.mp3, tema_superficie.mp3, mosaicos_escenario_32x32.png, jugador.json, mio.tmx, mosaicos_mario_enano_30x30.png, goomba.json, mosaicos_enemigos_32x32.png, mosaicos_enemigos_32x46.png, tortuga.json, tuberias.json, mosaicos_subway.png, tuberias.png, mundo1_subway.tmx";
 
 Q.loadTMX(recursos, function() {
 	//Carga las imagenes con sus respectivos archivos de configuracion JSON
 	Q.compileSheets("mosaicos_mario_enano_30x30.png", "jugador.json");
 	Q.compileSheets("mosaicos_enemigos_32x32.png", "goomba.json");
 	Q.compileSheets("mosaicos_enemigos_32x46.png", "tortuga.json");
+	Q.compileSheets("tuberias.png", "tuberias.json");
 	//Carga la escena
 	Q.stageScene("mundo1");
 	Q.stageScene("score", 1);
---
+++ juego/tuberiaEntrada.js
@@ -0,0 +1,15 @@
+//D57
+//Definimos la clase Jugador que se extiende la clase Sprite
+//Sprite pertenece al core de Quintu
+//nombre de la clase y objeto de configuración de la clase
+
+Q.Sprite.extend("TuberiaEntrada", {
+	//Funcion para inicializar
+	init: function(p){
+		this._super(p, {
+			sheet: "tuberias", //Configuracion JSON
+			frame: 2, //Que cuadro en la imagen es el que queremos tomar
+		});
+		this.add("2d"); //Añade controles (2d, controles, animaciones)	
+	}
+});
